cff5bb4fda3c1bd772886fdb72a10998a50c6260
snootched <snootched@gmail.com>
2024-01-22 19:49:04 -0500

    Update stt-stop.sh

--- snapcast/scripts/stt-stop.sh
+++ snapcast/scripts/stt-stop.sh
@@ -4,5 +4,6 @@ echo $(date) [stt-stop.sh] ...Starting stt-stop.sh script
 #insert a sleep if you like to tune the sound to feel more natural
 sleep 0.5
 
-echo $(date) [stt-stop.sh] ...Playing finish beep
-paplay --property=media.role=notification /home/pi/custom-sounds/stt-stop.wav 
+#purely for fun, this will play a sound after the user speech-to-text stage is done - computer is working :)
+echo $(date) [stt-stop.sh] ...Playing sound after speech input
+paplay --property=media.role=notification /home/pi/custom-sounds/stt-stop.wav
